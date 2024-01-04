14b2eb881aaa7ab693170d20120317ac26ba53d3
Eduardo C. Souza <edcampossouza@gmail.com>
2024-01-04 11:01:34 -0300

    basic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darker: "#075e54",
+        dark: "128c7e",
+        main: "#25D366",
+        light: "#dcf8c6",
+        lighter: "#ece5dd",
+      },
+    },
   },
   plugins: [],
 };
